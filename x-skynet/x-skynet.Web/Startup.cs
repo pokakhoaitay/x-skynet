3e8497888fe998752884cf201c8eea4612dc22df
pokakhoaitay <windowsphoneos@outlook.com>
2015-03-01 22:12:35 +0700

    init

---
+++ x-skynet/x-skynet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(x_skynet.Web.Startup))]
+namespace x_skynet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
